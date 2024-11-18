453769c242e4391a5498e8f4a20feeb7f484d19c
Johannes Pabst <johannes.pabst@ocilion.com>
2024-11-18 07:29:10 +0100

    2

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 BUILD_DIR=/opt/hd2/build/clang_hisi
 cmake -B $BUILD_DIR
-cmake --build $BUILD_DIR --config Release
-cmake --build $BUILD_DIR/CLANG-prefix/src/CLANG-build --target package
+#cmake --build $BUILD_DIR --config Release
+#cmake --build $BUILD_DIR/CLANG-prefix/src/CLANG-build --target package
+cmake --build $BUILD_DIR/CLANG-prefix/src/CLANG-build --target cxx --target cxxabi --target unwind
+
