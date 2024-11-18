36ccdbcbb5daf3907a484ee410cf54ff497f62ed
Johannes Pabst <johannes.pabst@ocilion.com>
2024-11-18 13:33:19 +0100

    4

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 BUILD_DIR=/opt/hd2/build/clang_hisi
 cmake -B $BUILD_DIR
-#cmake --build $BUILD_DIR --config Release
+cmake --build $BUILD_DIR --config Release
+#cmake --build $BUILD_DIR/CLANG-prefix/src/CLANG-build --target runtimes
 #cmake --build $BUILD_DIR/CLANG-prefix/src/CLANG-build --target package
-cmake --build $BUILD_DIR/CLANG-prefix/src/CLANG-build --target cxx --target cxxabi --target unwind
+#cmake --build $BUILD_DIR/CLANG-prefix/src/CLANG-build --target cxx --target cxxabi --target unwind
 
+
+#cmake -U_
+
+#cmake -DLIBUNWIND_ENABLE_SHARED:BOOL=OFF -DLIBCXX_ENABLE_SHARED:BOOL=OFF -DLIBCXXABI_ENABLE_SHARED:BOOL=OFF -DLIBCXX_ENABLE_ABI_LINKER_SCRIPT:BOOL=OFF  /opt/hd2/build/clang_hisi/CLANG-prefix/src/CLANG-build/runtimes/runtimes-arm-linux-gnueabihf-bins
+
+#cmake --build /opt/hd2/build/clang_hisi/CLANG-prefix/src/CLANG-build/runtimes/runtimes-arm-linux-gnueabihf-bins
